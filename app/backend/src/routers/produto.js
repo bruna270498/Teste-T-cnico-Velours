cc98a1474dc0c0ac0248dbdea47a6f5022f324aa
bruna <brunaalmeida075@gmail.com>
2023-06-25 12:59:47 -0300

    api criado com sucesso o metodo CRUD implementado

--- app/backend/src/routers/produto.js
+++ app/backend/src/routers/produto.js
@@ -4,7 +4,11 @@ const ControllerProduto = require('../controllers/produto');
 routerProduto = Router();
 produtoController = new ControllerProduto();
 
-routerProduto.get('/', produtoController.findAll)
-routerProduto.get('/:id', produtoController.findBy)
+routerProduto.get('/', produtoController.findAll);
+routerProduto.get('/:id', produtoController.findBy);
+routerProduto.put('/:id', produtoController.update);
+routerProduto.post('/', produtoController.newProduct);
+routerProduto.delete('/:id', produtoController.productDelet)
+
 
 module.exports = routerProduto;
